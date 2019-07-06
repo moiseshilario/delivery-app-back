6bccd8061d0951064db0002af0b95831ec65c65f
moiseshilario <moiseshilariu@gmail.com>
2019-07-06 12:49:41 -0300

    Separate private and public routes

--- src/routes.js
+++ src/routes.js
@@ -13,20 +13,32 @@ const TypeController = require('./app/controllers/TypeController')
 const FileController = require('./app/controllers/FileController')
 const OrderController = require('./app/controllers/OrderController')
 
+/**
+ * Public routes
+ */
 routes.get('/files/:file', FileController.show)
 routes.post('/files', upload.array('images'), FileController.store)
 
+routes.post('/session', SessionController.store)
+
 routes.get('/users', UserController.index)
-routes.get('/users/:id/orders', UserController.listOrders)
-routes.get('/users/:id/cart', UserController.getOpenCart)
 routes.post('/users', UserController.store)
 
 routes.get('/products', ProductController.index)
 routes.get('/products/:id/types', ProductController.listTypes)
-routes.post('/products', ProductController.store)
-routes.put('/products', ProductController.store)
 
 routes.get('/types/:id/prices', TypeController.listPrices)
+
+/**
+ * Private routes
+ */
+routes.use(authMiddleware)
+
+routes.get('/users/:id/orders', UserController.listOrders)
+routes.get('/users/:id/cart', UserController.getOpenCart)
+
+routes.post('/products', ProductController.store)
+routes.put('/products', ProductController.store)
 
 routes.get('/orders', OrderController.index)
 routes.post('/orders', OrderController.store)
@@ -35,9 +47,5 @@ routes.put('/orders/:id/confirm', OrderController.confirmOrder)
 routes.get('/orders/:id/items', OrderController.listOrderItems)
 routes.post('/orders/:id/items', OrderController.addItem)
 routes.delete('/orders/:id/items/:itemId', OrderController.removeItem)
-
-routes.post('/session', SessionController.store)
-
-routes.get('/teste', authMiddleware, (req, res) => res.json({ message: 'ok' }))
 
 module.exports = routes
